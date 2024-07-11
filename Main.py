f50595b16f66b87f9ff6f53dda248ae55a861d5f
“AngelPerez557” <aaperez@uth.hn>
2024-07-11 15:12:41 -0600

    Avances del proyecto de departamentos

--- Main.py
+++ Main.py
@@ -1,15 +1,16 @@
 import os
+import sys
+sys.path.append('AvanzadaII\Proyecto1\Clases')
 os.system('cls'if os.name == 'nt' else 'clear')
 
-from clases.Login import verificar_acceso 
-from clases.Menu import menu
+from Clases.Login import verificar_acceso 
+from Clases.Menu import menu
+from Clases.Opciones import opcionesMenu
 
 booleana = verificar_acceso()
 
 if booleana == True:
     n = menu()
-    print(n)
-
-
-
+    opciones = opcionesMenu()
+    opciones.opcion(n)
     
