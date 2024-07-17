64616ad94ebb10f6de652a945a55a599529b0d7e
“AngelPerez557” <aaperez@uth.hn>
2024-07-17 11:35:17 -0600

    Trabajo Finalizado

--- Main.py
+++ Main.py
@@ -1,16 +1,20 @@
 import os
 import sys
-sys.path.append('AvanzadaII\Proyecto1\Clases')
-os.system('cls'if os.name == 'nt' else 'clear')
+sys.path.append('C:\\Users\\jorge\\Desktop\\Inicio\\UTH\\2024\\2-2024\\AVANZADA_II\\II_P\\ControladorReconocimientoFacial\\.venv\\Clases')
 
-from Clases.Login import verificar_acceso 
-from Clases.Menu import menu
+from Clases.Login import Login
+from Clases.Menu import Menus
 from Clases.Opciones import opcionesMenu
 
-booleana = verificar_acceso()
+menus = Menus()
+login = Login()
 
-if booleana == True:
-    n = menu()
+m = menus.login_menu()
+booleana = False
+booleana = login.elegir_metodo(m)
+print(booleana)
+
+if booleana == True or booleana == None:
+    n = menus.menu()
     opciones = opcionesMenu()
     opciones.opcion(n)
-    
