0cf1c129426d055caacc05a89cf7dacd776b820b
Angel Antonio Pérez Rodriguez <aaperez@uth.hn>
2024-07-17 12:44:27 -0600

    Actualizar Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 import os
 import sys
-sys.path.append('C:\\Users\\jorge\\Desktop\\Inicio\\UTH\\2024\\2-2024\\AVANZADA_II\\II_P\\ControladorReconocimientoFacial\\.venv\\Clases')
+sys.path.append('C:\\Users\\angel\\Desktop\\Inicio\\UTH\\2024\\2-2024\\AVANZADA_II\\II_P\\ControladorReconocimientoFacial\\.venv\\Clases')
 
 from Clases.Login import Login
 from Clases.Menu import Menus
